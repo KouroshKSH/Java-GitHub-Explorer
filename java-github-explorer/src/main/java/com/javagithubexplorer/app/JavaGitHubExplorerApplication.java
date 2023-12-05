6072ad70cac05ac1138fd2d989376b35f236409c
LiLSchw4nz <lilschw4nz@gmail.com>
2023-12-05 16:54:08 +0330

    added the initializer files generated by Spring Initializer website

---
+++ java-github-explorer/src/main/java/com/javagithubexplorer/app/JavaGitHubExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.javagithubexplorer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGitHubExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGitHubExplorerApplication.class, args);
+	}
+
+}
