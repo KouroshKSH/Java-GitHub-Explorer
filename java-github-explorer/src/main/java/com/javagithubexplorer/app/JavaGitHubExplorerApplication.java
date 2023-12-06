f7a5d7796379c5e48d0a05153d9e9c590433a5ca
LiLSchw4nz <lilschw4nz@gmail.com>
2023-12-06 20:37:13 +0330

    added logger via slf4j for testing purposes

--- java-github-explorer/src/main/java/com/javagithubexplorer/app/JavaGitHubExplorerApplication.java
+++ java-github-explorer/src/main/java/com/javagithubexplorer/app/JavaGitHubExplorerApplication.java
@@ -1,12 +1,17 @@
 package com.javagithubexplorer.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaGitHubExplorerApplication {
+	private static final Logger logger = LoggerFactory.getLogger(JavaGitHubExplorerApplication.class);
 
 	public static void main(String[] args) {
+		System.out.println("\n\n>>>\tLet's try logging...\n\n");
+		logger.info("\n<!> first log test <!>\n");
 		SpringApplication.run(JavaGitHubExplorerApplication.class, args);
 	}
 
